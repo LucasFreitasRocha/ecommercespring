b431b33c15644368e7d7e4664d78b61610bdf64d
Lucas Rocha <rochadefreitaslucas@gmail.com>
2020-11-07 22:06:16 -0300

    add openfeign

---
+++ loja/src/main/java/com/loja/client/FornecedorClient.java
@@ -0,0 +1,16 @@
+package com.loja.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.loja.controller.dto.InfoFornecedorDTO;
+
+@FeignClient("fornecedor")
+public interface FornecedorClient {
+
+	@RequestMapping("/info/{estado}")
+	InfoFornecedorDTO getInfoPorEstado(@PathVariable String estado);
+
+	
+}
--- loja/src/main/java/com/loja/service/compraService.java
+++ loja/src/main/java/com/loja/service/compraService.java
@@ -1,26 +1,28 @@
 package com.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.loja.client.FornecedorClient;
 import com.loja.controller.dto.CompraDTO;
 import com.loja.controller.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
 	
+	/*@Autowired
+	private RestTemplate client;*/
+	
 	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClient;
 
 	public InfoFornecedorDTO  realizaCompra(CompraDTO compra) {
 		// TODO Auto-generated method stub
-	 ResponseEntity<InfoFornecedorDTO> info	=  client
+	/* ResponseEntity<InfoFornecedorDTO> info	=  client
 			 .exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(), 
 					 HttpMethod.GET, null, InfoFornecedorDTO.class);
-	 return info.getBody();
+	 return info.getBody();*/
+		return fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 	}
 
 }
